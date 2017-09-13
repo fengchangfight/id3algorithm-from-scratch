b7a0a2482ded19f0cc6555b2d9462c59ff3fda76
fengchang <fengchang_fight@163.com>
2017-09-13 13:58:42 +0800

    init id3 decision tree algorithm

---
+++ dtree.py
@@ -0,0 +1,152 @@
+"""
+==fc== ID3 algorithm, build decision tree
+"""
+import math
+
+# ==fc== Calculates the entropy of the given data set for the target attribute.
+def entropy(data, target_attr):
+    val_freq = {}
+    data_entropy = 0.0
+
+    # Calculate the frequency of each of the values in the target attr
+    for record in data:
+        if (record[target_attr] in val_freq):
+            val_freq[record[target_attr]] += 1.0
+        else:
+            val_freq[record[target_attr]] = 1.0
+
+    # Calculate the entropy of the data for the target attribute
+    for freq in val_freq.values():
+        data_entropy += (-freq/len(data)) * math.log(freq/len(data), 2)
+
+    return data_entropy
+
+# ==fc== calc information gain
+def gain(data, attr, target_attr):
+    val_freq = {}
+    subset_entropy = 0.0
+    for record in data:
+        if (record[attr] in val_freq):
+            val_freq[record[attr]] += 1.0
+        else:
+            val_freq[record[attr]] = 1.0
+    for val in val_freq.keys():
+        val_prob = val_freq[val] / sum(val_freq.values())
+        data_subset = [record for record in data if record[attr] == val]
+        subset_entropy += val_prob * entropy(data_subset, target_attr)
+    return (entropy(data, target_attr) - subset_entropy)
+
+#==fc== this is recursive method, which always choose the best attr and subdivide the dataset into sub dataset to make subtrees until no node
+def create_decision_tree(data, attributes, target_attr, fitness_func):
+    """
+    Returns a new decision tree based on the examples given.
+    """
+    data = data[:]
+    vals = [record[target_attr] for record in data]
+    default = majority_value(data, target_attr)
+    if not data or (len(attributes) - 1) <= 0:
+        return default
+    #==fc== all the same values
+    elif vals.count(vals[0]) == len(vals):
+        return vals[0]
+    else:
+        # ==fc== Choose the next best attribute to best classify our data
+        best = choose_attribute(data, attributes, target_attr,fitness_func)
+
+        # Create a new decision tree/node with the best attribute and an empty
+        # dictionary object--we'll fill that up next.
+        tree = {best: {}}
+
+        # ==fc== create sub tree
+        for val in get_values(data, best):
+            # ==fc== create sub tree
+            subtree = create_decision_tree(
+                get_examples(data, best, val),
+                [attr for attr in attributes if attr != best],
+                target_attr,
+                fitness_func)
+
+            tree[best][val] = subtree
+
+    return tree
+
+
+########################## Helper Functions #####################
+
+
+# ==fc== get the majority target value across the data set
+def majority_value(data, target_attr):
+    data = data[:]
+    return most_frequent([record[target_attr] for record in data])
+
+# ==fc== return the most frequently occured value
+def most_frequent(lst):
+    lst = lst[:]
+    highest_freq = 0
+    most_freq = None
+    for val in unique(lst):
+        if lst.count(val) > highest_freq:
+            most_freq = val
+            highest_freq = lst.count(val)
+    return most_freq
+
+# ==fc== remove duplicate
+def unique(lst):
+    lst = lst[:]
+    unique_lst = []
+    for item in lst:
+        if unique_lst.count(item) <= 0:
+            unique_lst.append(item)
+    return unique_lst
+
+# ==fc== remove duplicate attribute values
+def get_values(data, attr):
+    data = data[:]
+    return unique([record[attr] for record in data])
+
+# ==fc== choose the best attribute by best information gain, fitness is a helper function passed in
+def choose_attribute(data, attributes, target_attr, fitness):
+    data = data[:]
+    best_gain = 0.0
+    best_attr = None
+    for attr in attributes:
+        gain = fitness(data, attr, target_attr)
+        if (gain >= best_gain and attr != target_attr):
+            best_gain = gain
+            best_attr = attr
+    return best_attr
+
+# ==fc== get data has attr of specific value
+def get_examples(data, attr, value):
+    data = data[:]
+    rtn_lst = []
+
+    if not data:
+        return rtn_lst
+    else:
+        for record in data:
+            if record[attr] == value:
+                rtn_lst.append(record)
+
+        return rtn_lst
+
+#==fc== this is a recursive function to get classification of a data record
+def get_classification(record, tree):
+    if isinstance(tree, str):
+        return tree
+
+    # Traverse the tree further until a leaf node is found.
+    else:
+        attr = list(tree.keys())[0]
+        t = tree[attr][record[attr]]
+        return get_classification(record, t)
+
+#==fc== return a list of classifications of data
+def classify(tree, data):
+    data = data[:]
+    classification = []
+
+    for record in data:
+        classification.append(get_classification(record, tree))
+
+    return classification
---
+++ run.py
@@ -0,0 +1,135 @@
+# -*- coding:utf-8 -*-
+
+import sys
+import dtree
+
+
+#==fc==
+def get_training_file():
+    training_filename = './train.dat'
+    try:
+        fTrainIn = open(training_filename, "r")
+    except IOError:
+        print("Error: Could not find the training file specified or unable "
+              "to open it" % training_filename)
+        sys.exit(0)
+    return fTrainIn
+
+#==fc==
+def get_test_file():
+    test_filename = "./test.dat"
+    try:
+        fTestIn = open(test_filename, "r")
+    except IOError:
+        print("Error: Could not find the test file specified or unable to "
+              "open it" % test_filename)
+        sys.exit(0)
+    return fTestIn
+
+
+#==fc==
+def prepare_attributes(attrList):
+    attrList = attrList[:]
+    attrs = []
+    attrsDict = {}
+    for i in range(0, len(attrList)-1, 2):
+        attrs.append(attrList[i])
+        # set the value of attribute name as key to its number
+        attrsDict[attrList[i]] = attrList[i+1]
+    return attrs, attrsDict
+
+# ==fc==
+def run_app(fTrainIn, fTestIn):
+    #==fc== file lines into python list
+    linesInTest = [line.strip() for line in fTestIn.readlines()]
+    #==fc== collect attribute
+    attributes = linesInTest[0].split(" ")
+    #==fc== get rid of the header line
+    linesInTest.reverse()
+    linesInTest.pop()    # pop()弹出并返回最后一行
+    linesInTest.reverse()
+
+    attrList, attrDict = prepare_attributes(attributes)
+    targetAttribute = attrList[-1]
+
+    # prepare testdata
+    testData = []
+    for line in linesInTest:
+        testData.append(dict(list(zip(attrList,
+                                      [datum.strip()
+                                       for datum in line.split("\t")]))))
+
+    linesInTrain = [lineTrain.strip() for lineTrain in fTrainIn.readlines()]
+    attributesTrain = linesInTrain[0].replace("\t", " ").split(" ")
+
+    #==fc== do the same for training data
+    #once we have the attributes remove it from lines
+    linesInTrain.reverse()
+    linesInTrain.pop()   # pops from end of list, hence the two reverses
+    linesInTrain.reverse()
+
+    attrListTrain, attrDictTrain = prepare_attributes(attributesTrain)
+    targetAttrTrain = attrListTrain[-1]
+
+    # prepare data
+    trainData = []
+    for lineTrain in linesInTrain:
+        trainData.append(dict(list(zip(attrListTrain,
+                                       [datum.strip()
+                                        for datum in lineTrain.split("\t")]))))
+
+    trainingTree = dtree.create_decision_tree(trainData, attrListTrain,targetAttrTrain, dtree.gain)
+
+    trainingClassify = dtree.classify(trainingTree, trainData)
+
+    testTree = dtree.create_decision_tree(testData, attrList, targetAttribute,dtree.gain)
+
+    testClassify = dtree.classify(testTree, testData)
+
+    # also returning the example Classify in both the files
+    givenTestClassify = []
+    for row in testData:
+        givenTestClassify.append(row[targetAttribute])
+
+    givenTrainClassify = []
+    for row in trainData:
+        givenTrainClassify.append(row[targetAttrTrain])
+
+    return (trainingTree, trainingClassify, testClassify, givenTrainClassify,
+            givenTestClassify)
+
+#==fc== calculate matched/total
+def accuracy(algoClassify, targetClassify):
+    matching_count = 0.0
+    for alg, target in zip(algoClassify, targetClassify):
+        if alg == target:
+            matching_count += 1.0
+    # print len(algoClassify)
+    # print len(targetClassify)
+    return (matching_count / len(targetClassify)) * 100
+
+
+#==fc== print tree
+def print_tree(tree, str):
+    if isinstance(tree, dict):
+        # print("%s%s = " % (str, list(tree.keys())[0]))
+        for item in list(list(tree.values())[0].keys()):
+            print("  |  %s%s = %s" % (str, list(tree.keys())[0], item))
+            print_tree(list(tree.values())[0][item], str + "   ")
+            # the space in 'str + "   "' affect Backspace between Sub-layer
+    else:
+        print("   -->  %s : %s" % (str, tree))
+        # --> stand for the targetAttribute
+
+#==fc== program entry point
+if __name__ == "__main__":
+    fTrainIn = get_training_file()
+    fTestIn = get_test_file()
+    (trainingTree, trainingClassify, testClassify, givenTrainClassify,givenTestClassify) = run_app(fTrainIn, fTestIn)
+    print_tree(trainingTree, "")
+    print(" Accuracy of training set (%s instances) :  %s"
+          % (len(givenTrainClassify),
+             accuracy(trainingClassify, givenTrainClassify)))
+    print(" Accuracy of test set (%s instances) :  %s"
+          % (len(givenTestClassify),
+             accuracy(testClassify, givenTestClassify)))
